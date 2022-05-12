41064cae28c4eb48813769d53a2717e42ad06d3a
KamuiKazi <93505258+KamuiKazi@users.noreply.github.com>
2022-05-12 14:21:22 -0500

    Add files via upload

---
+++ GoGame/GoGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoGame
+//
+//  Created by period4 on 11/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
